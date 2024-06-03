614d5ef1af4abf7e4ad3dc6832027108adb86164
g-otn <44736064+g-otn@users.noreply.github.com>
2024-06-03 07:33:53 -0300

    feat: cleanup names and tags

--- vpc.tf
+++ vpc.tf
@@ -6,19 +6,26 @@ module "vpc" {
 
   azs             = [var.subnet_az]
   public_subnets  = ["10.0.101.0/24"]
-  public_subnet_names = ["Public subnet 1 (${var.subnet_az})"]
+  public_subnet_names = ["${local.title} Public Subnet 1 (${var.subnet_az})"]
 
   enable_flow_log = true
   flow_log_cloudwatch_log_group_retention_in_days = 30
+  flow_log_cloudwatch_log_group_name_prefix = "/aws/vpc-flow-log/${local.title_PascalCase}-"
 
   create_flow_log_cloudwatch_iam_role = true
   create_flow_log_cloudwatch_log_group = true
+
+  public_route_table_tags ={
+    Name = "${local.title} Public Route Table"
+  }
 
   igw_tags = {
     Name = "${local.title} Internet Gateway"
   }
 
+  default_route_table_name = "${local.title} Default Route Table"
   default_network_acl_name = "${local.title} Default Network ACL"
+  default_security_group_name = "${local.title} Default Security Group" 
 }
 
 resource "aws_security_group" "spot_instance" {

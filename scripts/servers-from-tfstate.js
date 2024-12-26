65b9e05421ddf73219ca7965639d114db33881cc
g-otn <44736064+g-otn@users.noreply.github.com>
2024-12-26 00:14:13 -0300

    fix: use terraform show instead of reading .tfstate manually
    unblock from using remote backend

--- scripts/servers-from-tfstate.js
+++ scripts/servers-from-tfstate.js
@@ -1,44 +1,53 @@
 const fs = require('fs');
 const execSync = require('child_process').execSync;
 
-const resourceRegex =
-  /^(.+\.module\.ec2_spot_instance\.aws_spot_instance_request\..+)$/gm;
+console.log('Retrieving terraform state');
+const state = execSync('terraform show -json -no-color', { encoding: 'utf-8' });
 
-console.log('Listing terraform state');
-const output = execSync('terraform state list', { encoding: 'utf-8' });
+// See https://developer.hashicorp.com/terraform/internals/json-format
+console.log('Parsing state');
+const child_modules = JSON.parse(state).values.root_module.child_modules;
 
-const serverResources = [...output.matchAll(resourceRegex)].map((m) => m[1]);
+console.log('Child modules found:', child_modules.length);
+const server_modules = child_modules.filter((m) =>
+  m.child_modules?.some((cm) =>
+    cm.resources?.some(
+      (r) =>
+        r.type === 'aws_spot_instance_request' &&
+        r.values.tags?.['GameServerEC2Discord:ServerId']
+    )
+  )
+);
 
-console.log('Server spot instance requests found:', serverResources);
-const serversIdRegex = /"GameServerEC2Discord:ServerId".+"(.+)"/;
-const serversRegionRegex = /"GameServerEC2Discord:Region".+"(.+)"/;
+console.log('Server modules found:');
+console.log(server_modules.map((s) => `- ${s.address}`).join('\n'));
 
-const stateData = serverResources.map((r, i) => {
-  console.log('Showing resource', i, r);
-  const resourceOutput = execSync('terraform state show ' + r, {
-    encoding: 'utf-8',
-  });
+const serverConfig = server_modules.map((sm) => {
+  console.log('Getting config for server module', sm.address);
+  const spotRequestTags = sm.child_modules.reduce((prev, curr) => {
+    console.log(`prev, curr`, prev, curr);
+    if (prev) {
+      return prev;
+    }
 
-  const serverId = resourceOutput.match(serversIdRegex)?.[1];
-  const region = resourceOutput.match(serversRegionRegex)?.[1];
+    return curr.resources?.find(
+      (r) =>
+        r.type === 'aws_spot_instance_request' &&
+        r.values.tags?.['GameServerEC2Discord:ServerId']
+    );
+  }, null).values.tags;
 
-  console.log('Found for', i, { serverId, region });
-
-  return { serverId, region };
+  return {
+    region: spotRequestTags['GameServerEC2Discord:Region'],
+    gameServerId: spotRequestTags['GameServerEC2Discord:ServerId'],
+    discordGuildId: '<Discord Guild ID here>',
+    choiceDisplayName: spotRequestTags['GameServerEC2Discord:ServerId'],
+  };
 });
 
-console.log('server data found', stateData);
+console.log('Generate Discord script server config:', serverConfig);
 
-const fileData = JSON.stringify(
-  stateData.map(({ region, serverId }) => ({
-    region: region || '<AWS Region here>',
-    gameServerId: serverId || '<Game Server ID here>',
-    discordGuildId: '<Discord Guild ID here>',
-    choiceDisplayName: '<Choice display name here>',
-  })),
-  null,
-  '  '
-);
+const fileData = JSON.stringify(serverConfig, null, 2);
 
 fs.writeFileSync('scripts/servers.json', fileData);
 console.log('scripts/servers.json created');
